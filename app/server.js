3de84d89491cae6d3996c6979038731e49bd299f
Chris <christopherdakin1@gmail.com>
2018-01-07 22:14:37 -0600

    Fixes server

--- app/server.js
+++ app/server.js
@@ -5,13 +5,13 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname + "/../app/index.html"))
+    res.sendFile(path.resolve(__dirname + "/app/index.html"))
 );
 
-app.use(express.static(path.resolve(__dirname + "/../app/"))); //Serves resources from public folder
+app.use(express.static(path.resolve(__dirname + "/app/"))); //Serves resources from public folder
 
 app.get("/app.css", (req, res) =>
-    res.sendFile(path.resolve(__dirname + "/../app/app.css"))
+    res.sendFile(path.resolve(__dirname + "/app/app.css"))
 );
 
 app.listen(port, () =>
